6e107e72faac257120578b061b7a56ee5978f9bd
Dmitri Grabov <d.grabov@gmail.com>
2023-05-22 23:17:34 +0100

    Add TS to reducers

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,188 +1,188 @@
 export type ZoomLevel = {
-  label: string;
-  duration: number;
-};
+  label: string
+  duration: number
+}
 
 export type Dimensions = {
-  ticks: number;
-  height: number;
-  width: number;
-  marginLeft: number;
-  marginTop: number;
-  marginBottom: number;
-  contentHeight: number;
-  width_controls: number;
-};
+  ticks: number
+  height: number
+  width: number
+  marginLeft: number
+  marginTop: number
+  marginBottom: number
+  contentHeight: number
+  width_controls: number
+}
 
 export type Timeline = {
-  dimensions: Dimensions;
-  range: [Date, Date];
-  rangeLimits: [Date, Date];
-  zoomLevels: ZoomLevel[];
-};
+  dimensions: Dimensions
+  range: [Date, Date]
+  rangeLimits: [Date, Date]
+  zoomLevels: ZoomLevel[]
+}
 
 export type Cluster = {
-  radius: number;
-  minZoom: number;
-  maxZoom: number;
-};
+  radius: number
+  minZoom: number
+  maxZoom: number
+}
 
 export type Views = {
-  events: boolean;
-  routes: boolean;
-  sites: boolean;
-};
+  events: boolean
+  routes: boolean
+  sites: boolean
+}
 
 export type Associations = {
-  coloringSet: unknown[];
-  filters: unknown[];
-  narrative: unknown;
-  categories: unknown[];
-  views: Views;
-};
+  coloringSet: unknown[]
+  filters: unknown[]
+  narrative: unknown
+  categories: unknown[]
+  views: Views
+}
 
 export type Errors = {
-  source: boolean;
-};
+  source: boolean
+}
 
 export type Flags = {
-  isFetchingDomain: boolean;
-  isFetchingSources: boolean;
-  isCover: boolean;
-  isCardstack: boolean;
-  isInfopopup: boolean;
-  isIntropopup: boolean;
-  isShowingSites: boolean;
-};
+  isFetchingDomain: boolean
+  isFetchingSources: boolean
+  isCover: boolean
+  isCardstack: boolean
+  isInfopopup: boolean
+  isIntropopup: boolean
+  isShowingSites: boolean
+}
 
 export type Cover = {
-  title: string;
-  description: string;
-  exploreButton: string;
-};
+  title: string
+  description: string
+  exploreButton: string
+}
 
 export type Category = {
-  icon: string;
-  label: string;
-  title: string;
-  description: string;
-};
+  icon: string
+  label: string
+  title: string
+  description: string
+}
 
 export type Categories = {
-  default: Category;
-};
+  default: Category
+}
 
 export type Panels = {
-  categories: Categories;
-  filters: Category;
-  narratives: Category;
-  shapes: Category;
-};
+  categories: Categories
+  filters: Category
+  narratives: Category
+  shapes: Category
+}
 
 export type Toolbar = {
-  panels: Panels;
-};
+  panels: Panels
+}
 
 export type Map = {
-  anchor: [number, number];
-  startZoom: number;
-};
+  anchor: [number, number]
+  startZoom: number
+}
 
 export type App = {
-  debug: boolean;
-  errors: Errors;
-  highlighted: unknown;
-  selected: unknown[];
-  source: unknown;
-  associations: Associations;
-  shapes: unknown[];
-  isMobile: boolean;
-  language: "en-US";
-  cluster: Cluster;
-  timeline: Timeline;
-  flags: Flags;
-  cover: Cover;
-  toolbar: Toolbar;
-  loading: boolean;
-  map: Map;
-};
+  debug: boolean
+  errors: Errors
+  highlighted: unknown
+  selected: unknown[]
+  source: unknown
+  associations: Associations
+  shapes: unknown[]
+  isMobile: boolean
+  language: 'en-US'
+  cluster: Cluster
+  timeline: Timeline
+  flags: Flags
+  cover: Cover
+  toolbar: Toolbar
+  loading: boolean
+  map: Map
+}
 
 export type Domain = {
-  events: unknown[];
-  categories: unknown[];
-  associations: unknown[];
-  sources: unknown;
-  sites: unknown[];
-  shapes: unknown[];
-  regions: unknown[];
-  notifications: unknown[];
-};
+  events: unknown[]
+  categories: unknown[]
+  associations: unknown[]
+  sources: unknown
+  sites: unknown[]
+  shapes: unknown[]
+  regions: unknown[]
+  notifications: unknown[]
+}
 
 export type Titles = {
-  current: string;
-  default: string;
-};
+  current: string
+  default: string
+}
 
 export type Narratives = {
-  opacity: number;
-  stroke: string;
-  strokeWidth: number;
-};
+  opacity: number
+  stroke: string
+  strokeWidth: number
+}
 
 export type Style = {
   categories: {
-    default: string;
-  };
+    default: string
+  }
   narratives: {
-    default: Narratives;
-  };
+    default: Narratives
+  }
   regions: {
-    default: Narratives;
-  };
+    default: Narratives
+  }
   clusters: {
-    radial: false;
-  };
-};
+    radial: false
+  }
+}
 
 export type Card = {
   layout: {
-    template: string;
-  };
-};
+    template: string
+  }
+}
 
 export type Coloring = {
-  maxNumOfColors: number;
-  colors: string[];
-};
+  maxNumOfColors: number
+  colors: string[]
+}
 
 export type Dom = {
-  timeline: string;
-  timeslider: string;
-  map: string;
-};
+  timeline: string
+  timeslider: string
+  map: string
+}
 
 export type Ui = {
-  tiles: Titles;
-  style: Style;
-  card: Card;
-  coloring: Coloring;
-  dom: Dom;
-  eventRadius: number;
-};
+  tiles: Titles
+  style: Style
+  card: Card
+  coloring: Coloring
+  dom: Dom
+  eventRadius: number
+}
 
 export type Features = {
-  USE_COVER: boolean;
-  USE_ASSOCIATIONS: boolean;
-  USE_SITES: boolean;
-  USE_SOURCES: boolean;
-  USE_REGIONS: boolean;
-  GRAPH_NONLOCATED: boolean;
-  HIGHLIGHT_GROUPS: boolean;
-};
+  USE_COVER: boolean
+  USE_ASSOCIATIONS: boolean
+  USE_SITES: boolean
+  USE_SOURCES: boolean
+  USE_REGIONS: boolean
+  GRAPH_NONLOCATED: boolean
+  HIGHLIGHT_GROUPS: boolean
+}
 
 export type AppState = {
-  domain: Domain;
-  app: App;
-  ui: Ui;
-  features: Features;
-};
+  domain: Domain
+  app: App
+  ui: Ui
+  features: Features
+}
