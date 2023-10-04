45325917d469e7f24a0d07ac1bfbdb05a3018b63
Dmitri Grabov <d.grabov@gmail.com>
2023-10-04 17:29:21 +0100

    tweak

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./components/App";
 import 'video-react/dist/video-react.css'
+
 createRoot(globalThis.window.document.getElementById('explore-app')).render(
   <Provider store={store}>
     <App />
