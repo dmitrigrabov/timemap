1c49185e98731fdb7425320ec5419b1fa2f3d13b
Dmitri Grabov <d.grabov@gmail.com>
2023-10-04 17:01:39 +0100

    CRA to Vite + fixes

---
+++ temp.ts
@@ -0,0 +1,46 @@
+const association = {
+  "filter_paths": [
+      "Type of incident",
+      "Unlawful attacks on civilians"
+  ],
+  "id": "CIVILIANS",
+  "title": "",
+  "desc": "CIVILIANS",
+  "mode": "FILTER"
+}
+
+const event = {
+  "associations": [
+      {
+          "filter_paths": [
+              "Type of incident",
+              "Unlawful attacks on civilians"
+          ],
+          "id": "CIVILIANS",
+          "title": "",
+          "desc": "CIVILIANS",
+          "mode": "FILTER"
+      },
+      {
+          "filter_paths": [
+              "Means of attack",
+              "Missile fire"
+          ],
+          "id": "MISSILE_FIRE",
+          "title": "",
+          "desc": "MISSILE_FIRE",
+          "mode": "FILTER"
+      }
+  ],
+  "sources": [
+      "https://t.me/ressentiment_channel/15782"
+  ],
+  "id": 0,
+  "description": "Cyclist killed in explosion",
+  "date": "24/02/2022",
+  "time": "",
+  "location": "Uman, Cherkasy Oblast",
+  "latitude": "48.7494",
+  "longitude": "30.2214",
+  "datetime": "2022-02-24T00:00:00.000Z"
+}
